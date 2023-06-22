73a8e357d8689b8b314aa3b11d650c13ce979334
Ray <ray.zhou@sparticle.com>
2023-06-22 20:53:50 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import Link from 'next/link'
 
-const Home = () => {
+export const runtime = 'edge';
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const getRandomname = () => {
+    return fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(data => data.results[0].name.first)
+}
+
+const getName = async () =>  {
+    return await delay(1000).then(() => getRandomname());
+}
+
+const Home = async () => {
+  const name = await getName();
+
   return (
     <div>
       <h1>worker-next-app</h1>
       <p>Hello World! This is the Home page</p>
+      <p>Random name: {name}</p>
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
